f4fa83f517871c998c906279dfee344f9fe70e94
sunhwa lee <2017sunhwa@gmail.com>
2021-01-23 14:15:28 +0900

    no message

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,21 @@ module.exports = {
   preset: 'react-native',
   verbose: true,
   setupFilesAfterEnv: ['./jest.setup.js'],
-
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir>/assetsTransformer.js'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|)'
+  ]
 };
 
 
