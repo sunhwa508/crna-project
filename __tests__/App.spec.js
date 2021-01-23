500e83a87fd31ccc41687237b8ff3286470bfb98
sunhwa lee <2017sunhwa@gmail.com>
2021-01-23 13:59:02 +0900

    setting jest test

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -2,7 +2,6 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
-
 // Note: test renderer must be required after react-native.
 
 it('renders correctly', () => {
